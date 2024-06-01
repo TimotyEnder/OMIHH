47898be00609665362c1b82f0c9157000cfccbac
jorlazar <124343717+jorlazar@users.noreply.github.com>
2024-06-02 00:28:20 +0200

    Rock hp
    Rock hp :)

---
+++ Assets/Scripts/Rock/RockHp.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockHp : MonoBehaviour
+{
+    [SerializeField] private int curHealth = 0;
+    [SerializeField] private int maxHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DamageRock(1);
+    }
+
+    public void DamageRock( int damage )
+    {
+        curHealth -= damage;
+    }
+}
