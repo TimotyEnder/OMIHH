85b4ed502736a22b7bfdce609acbf70a3f0f697d
TimotyEnder <timotyender@gmail.com>
2024-06-04 14:20:04 +0200

    destroy projectiles in x seconds

--- Assets/Scripts/Enemies/RangedEnemyLogic.cs
+++ Assets/Scripts/Enemies/RangedEnemyLogic.cs
@@ -7,12 +7,14 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private GameObject projectilePre;
     public int delay;
     private float speed;
+    private int enemyDestTime;
     [SerializeField] private GameObject shootPos;
     // Start is called before the first frame update
     void Start()
     {
         delay = 0;
         speed = 3f;
+        enemyDestTime = 5;
     }
 
     // Update is called once per frame
@@ -21,8 +23,14 @@ public class NewBehaviourScript : MonoBehaviour
         delay++;
         if(delay % 50 == 0){
             GameObject projectile = Instantiate(projectilePre, shootPos.transform.position, Quaternion.identity);
+            StartCoroutine(projDestroyCoroutine(projectile));
             Rigidbody2D body = projectile.GetComponent<Rigidbody2D>();
             body.velocity = shootPos.transform.right * speed;
         }
+    }
+    private IEnumerator projDestroyCoroutine(GameObject target) 
+    {
+        yield return new WaitForSeconds(enemyDestTime);
+        Destroy(target);
     }
 }
